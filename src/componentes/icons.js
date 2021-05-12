9d6d671a45e8203537cba9c982ad69123c630fa2
Gabriel Gleyser <ggleyser@outlook.com>
2021-05-12 15:18:11 -0300

    mudança no icone, e links

--- src/componentes/icons.js
+++ src/componentes/icons.js
@@ -8,9 +8,9 @@ export default function Icons(props)
     
     return(
         <div className={props.class}>
-            <a href="#face"><span className="m-2"><img src={icon_face} width="30"></img></span></a>
-            <a href="#face"><span className="m-2"><img src={icon_insta} width="30"></img></span></a>
-            <a href="#face"><span className="m-2"><img src={icon_twt} width="30"></img></span></a>
+            <a href="http://facebook.com/gabriel.gleyserdasilva/" target="_blank"><span className="m-2"><img src={icon_face} width="30"></img></span></a>
+            <a href="http://instagram.com/ggleyser" target="_blank"><span className="m-2"><img src={icon_insta} width="30"></img></span></a>
+            <a href="https://twitter.com/GleyserGabriel" target="_blank"><span className="m-2"><img src={icon_twt} width="30"></img></span></a>
         </div>
     )
 }
--- src/componentes/menu/nav.js
+++ src/componentes/menu/nav.js
@@ -6,7 +6,9 @@ export default function Nav()
         <nav className="navbar navbar-expand-lg navbar-light py-3" style={{ backgroundColor: "rgb(218 218 218)"}}>
             <div className="container">
 
-                <a className="navbar-brand h1" href="#inicio">LOGO</a>
+                <a className="navbar-brand h1" href="#inicio">
+                    <text>GGleyser_dev</text>
+                </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
